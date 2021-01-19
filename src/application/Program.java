c135c70bc8822865d6a41016cc963cc947d5228e
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2021-01-19 11:57:13 -0300

    Aplicando Orientacao a Objeto no Jogo da Velha. (Exercicio 4 Aula 27 Curso de Java Basico Loiane Groner)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,108 +1,68 @@
 package application;
 
 import java.util.Scanner;
+import layerboard.Board;
 
 public class Program {
+
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
-        char[][] board = new char[3][3]; //declarando matriz e instanciando a matriz 3x3
+
+        Board board = new Board(1);
         boolean win = false;
-        
+        char sinal;
+        int linha = 0, coluna = 0;
+
         System.out.println("Jogador 1 = X");
         System.out.println("Jogador 2 = O");
         
-        //iniciar tabuleiro
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board.length; j++){
-                board[i][j] = ' ';
-            }
-        }
-        /*
-            loop do jogo
-                pedir para entrar com uma jogada
-                ler informacao
-                verificar se espaco esta livre
-                    sim - adiciona valor
-                    nao - da erro e apresenta de novo
-                como finalizar o jogo (?)
-        */
-        
-        int turn = 1;
-        char sinal;
-        int linha=0,coluna=0;
-        System.out.println();
-        while(!win){
-            //Verificando de quem é a vez
-            if(turn % 2 == 1) {
+        while (!win) {
+
+            if (board.turn % 2 == 1) {
                 System.out.println("Jogador 1. Escolha a linha e coluna (1-3).");
                 sinal = 'X';
             } else {
-                System.out.println("Jogador 2.  Escolha a linha e coluna (1-3).");
+                System.out.println("Jogador 2. Escolha a linha e coluna (1-3).");
                 sinal = 'O';
             }
-            
-            //Lendo Linha
+
+            //Ler linha
             boolean linhaValida = false;
-            while(!linhaValida){
+            while (!linhaValida) {
                 System.out.print("Informe linha: ");
                 linha = input.nextInt();
-                if(linha >= 1 && linha < 4){
+                if (linha >= 1 && linha < 4) {
                     linhaValida = true;
-                }else{
+                } else {
                     System.out.println("Linha Invalida. Tente Novamente");
                 }
             }
-            //Lendo Coluna
+
+            //Ler coluna
             boolean colunaValida = false;
-            while(!colunaValida){
+            while (!colunaValida) {
                 System.out.print("Informe coluna: ");
                 coluna = input.nextInt();
-                if(coluna >= 1 && coluna < 4){
+                if (coluna >= 1 && coluna < 4) {
                     colunaValida = true;
-                }else{
+                } else {
                     System.out.println("Coluna Invalida. Tente Novamente");
                 }
             }
-            --linha;
-            --coluna;
-            if(board[linha][coluna] == ' '){
-                board[linha][coluna] = sinal;
-                turn++;
-            }else{
+
+            if (!board.validarJogada(--linha, --coluna, sinal)) {
                 System.out.println("Posição já usada. Tente Novamente");
             }
-            
-            for(int i=0; i<board.length; i++){
-                for(int j=0; j<board.length; j++){
-                    System.out.print(board[i][j] + " | ");
-                }
-                System.out.println();
-            }
-            
-            if((board[0][0] == 'X')&&(board[0][1] == 'X')&&(board[0][2] == 'X')||
-               (board[1][0] == 'X')&&(board[1][1] == 'X')&&(board[1][2] == 'X')||
-               (board[2][0] == 'X')&&(board[2][1] == 'X')&&(board[2][2] == 'X')||
-               (board[0][0] == 'X')&&(board[1][0] == 'X')&&(board[2][0] == 'X')||
-               (board[0][1] == 'X')&&(board[1][1] == 'X')&&(board[2][1] == 'X')||
-               (board[0][2] == 'X')&&(board[1][2] == 'X')&&(board[2][2] == 'X')) {
-                
-                System.out.println("Game 1 is Winner!");
-                win = true;
-                
-            }else if((board[0][0] == 'O')&&(board[0][1] == 'O')&&(board[0][2] == 'O')||
-               (board[1][0] == 'O')&&(board[1][1] == 'O')&&(board[1][2] == 'O')||
-               (board[2][0] == 'O')&&(board[2][1] == 'O')&&(board[2][2] == 'O')||
-               (board[0][0] == 'O')&&(board[1][0] == 'O')&&(board[2][0] == 'O')||
-               (board[0][1] == 'O')&&(board[1][1] == 'O')&&(board[2][1] == 'O')||
-               (board[0][2] == 'O')&&(board[1][2] == 'O')&&(board[2][2] == 'O')) {
-                System.out.println("Game 2 is Winner!");
-                win = true;
 
-            }else if(turn>9){
-                System.out.println("No Winner!");
+            board.showBoard();
+            win = board.verificarGanhador(sinal);
+            if(win==true && sinal == 'X'){
+                System.out.println("GAMER 1 IS WINNER!!");
+            }
+            else if(win==true && sinal == 'O'){
+                System.out.println("GAMER 2 IS WINNER!!");
             }
         }
-        
     }
 }
---
+++ src/layerboard/Board.java
@@ -0,0 +1,63 @@
+package layerboard;
+
+public class Board {
+    
+    public char[][] board;
+    public int turn;
+    
+    public Board (){
+        
+    }
+    
+    public Board(int value){
+        this.board = new char [3][3];
+        iniciarBoard();
+        this.turn = value;
+    }
+    
+    private void iniciarBoard(){
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board.length; j++){
+                board[i][j] = ' ';
+            }
+        }
+    }
+    
+    public boolean verificarEspacoLivre(int linha,int coluna){        
+        return board[linha][coluna]== ' ';
+    }
+    
+    public boolean  validarJogada(int linha,int coluna, char sinal){
+        if(verificarEspacoLivre(linha, coluna)){
+            board[linha][coluna] = sinal;
+            turn++;
+            return true;
+        }
+        return false;
+    }
+    
+    public void showBoard(){
+        for (char[] board1 : board) {
+            for (int j = 0; j<board.length; j++) {
+                System.out.print(board1[j] + " | ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public boolean verificarGanhador(char sinal){
+        if ((board[0][0] == sinal) && (board[0][1] == sinal) && (board[0][2] == sinal)//linha 1
+                || (board[1][0] == sinal) && (board[1][1] == sinal) && (board[1][2] == sinal) //linha 2
+                || (board[2][0] == sinal) && (board[2][1] == sinal) && (board[2][2] == sinal)//linha 3
+                || (board[0][0] == sinal) && (board[1][0] == sinal) && (board[2][0] == sinal)//coluna 1
+                || (board[0][1] == sinal) && (board[1][1] == sinal) && (board[2][1] == sinal)//coluna 2
+                || (board[0][2] == sinal) && (board[1][2] == sinal) && (board[2][2] == sinal)//coluna 3
+                || (board[0][0] == sinal) && (board[1][1] == sinal) && (board[2][2] == sinal)// diagonal
+                || (board[0][2] == sinal) && (board[1][1] == sinal) && (board[2][0] == sinal)//diagonal inversa
+                ) {
+            return true;
+        }
+        return false;
+    }
+    
+}
